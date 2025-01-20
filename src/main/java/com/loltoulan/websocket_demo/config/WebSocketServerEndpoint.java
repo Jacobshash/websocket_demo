c3cb065d7540ddce2bc2f2563342d5ecd62f99c3
LOL_toulan <lol_toulan@163.com>
2025-01-20 17:03:27 +0800

    feat(websocket): 添加 WebSocket 功能
    - 更新项目依赖版本： - spring-boot-starter-parent 从 3.3.7 降级到 2.7.14
      - redis 依赖被移除
      - lombok 版本升级到 1.18.30，范围从 optional改为 provided
      - spring-boot-starter-websocket 依赖被添加
      - maven-compiler-plugin 配置更新
      - spring-boot-maven-plugin 配置简化
    
    - 新增 WebSocket 配置类和服务器端点：
      - WebsocketConfig 配置类，启用 WebSocket 功能
      - WebSocketServerEndpoint 类，实现 WebSocket 服务器端点

---
+++ src/main/java/com/loltoulan/websocket_demo/config/WebSocketServerEndpoint.java
@@ -0,0 +1,55 @@
+package com.loltoulan.websocket_demo.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@Component
+@ServerEndpoint("/loltoulanWs")
+@Slf4j
+public class WebSocketServerEndpoint {
+
+    /**
+     * 连接成功
+     *
+     * @param session
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        log.info("session id is {} ,连接成功", session.getId());
+        log.info("连接成功");
+    }
+
+    /**
+     * 连接关闭
+     *
+     * @param session
+     */
+    @OnClose
+    public void onClose(Session session) {
+        log.info("session id is {} ,连接关闭", session.getId());
+        log.info("连接关闭");
+    }
+
+    /**
+     * 接收到消息
+     *
+     * @param text
+     */
+    @OnMessage
+    public String onMsg(String text,Session session) {
+        log.info("servet {}, 接收到消息：{}",session.getId(), text);
+        return "servet 发送：" + text;
+    }
+
+
+
+
+}
